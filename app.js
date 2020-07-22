dafb3f5c3e7ae6a18caf9edc76a903c0846b156b
mianfremo <xmianfremox@gmail.com>
2020-07-21 22:54:26 -0400

    Instalacion de body parser

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const api = require('./routes');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/api', api);
 
 
